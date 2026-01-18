245de82c06da6303f2e245a6d1992e61c2c34ff3
Ryan Ngo <ryanvngo@gmail.com>
2026-01-17 21:43:23 -0600

    Removed project component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
       </div>
